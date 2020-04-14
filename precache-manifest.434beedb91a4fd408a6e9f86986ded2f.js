f0eaaa8d5ea3559fbd67b00cc853b11cd34826b7
santiagop06 <santiagopuentes8@gmail.com>
2020-04-14 10:20:20 -0500

    Updates

---
+++ precache-manifest.434beedb91a4fd408a6e9f86986ded2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5435f521057d796e18879a04402477",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "469fcbe79ae70e462b9d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "af7c4358c11e061943bc",
+    "url": "/robofriends/static/css/main.7f08a19a.chunk.css"
+  },
+  {
+    "revision": "469fcbe79ae70e462b9d",
+    "url": "/robofriends/static/js/2.5e9ad4ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.5e9ad4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7c4358c11e061943bc",
+    "url": "/robofriends/static/js/main.1e607938.chunk.js"
+  },
+  {
+    "revision": "2d9291f9f5c10a633e67",
+    "url": "/robofriends/static/js/runtime-main.9955acc8.js"
+  }
+]);
