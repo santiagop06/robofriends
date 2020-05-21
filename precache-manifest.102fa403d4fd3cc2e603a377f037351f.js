9c78a68cfe241933eca5c010caa979be7dfa949f
santiagop06 <santiagopuentes8@gmail.com>
2020-05-21 08:58:47 -0500

    Updates

---
+++ precache-manifest.102fa403d4fd3cc2e603a377f037351f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24edf8f53f5431e7b18d872a90ca2f1",
+    "url": "/backgroundchange/index.html"
+  },
+  {
+    "revision": "d171651a726d1cd4f8d8",
+    "url": "/backgroundchange/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "58cc5b88d55ec9e9187a",
+    "url": "/backgroundchange/static/css/main.e0953ee8.chunk.css"
+  },
+  {
+    "revision": "d171651a726d1cd4f8d8",
+    "url": "/backgroundchange/static/js/2.0839f802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/backgroundchange/static/js/2.0839f802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cc5b88d55ec9e9187a",
+    "url": "/backgroundchange/static/js/main.9f8b1f05.chunk.js"
+  },
+  {
+    "revision": "9968eb3e5930272b3cce",
+    "url": "/backgroundchange/static/js/runtime-main.209a40e1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.25dd98392470ae2a818a2c2e21f7b321.js"
+  "/backgroundchange/precache-manifest.102fa403d4fd3cc2e603a377f037351f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/backgroundchange/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
